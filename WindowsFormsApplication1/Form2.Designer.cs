522456707a20f597b17393f72e79bbfcf6635bb3
John Cetin <NobleElemental@gmail.com>
2017-06-13 22:01:50 -0400

    First Push
    First files for the application. WIP

---
+++ WindowsFormsApplication1/Form2.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.jobTimerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet1 = new WindowsFormsApplication1.Database1DataSet1();
+            this.jobTimerBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.jobTimerTableAdapter1 = new WindowsFormsApplication1.Database1DataSet1TableAdapters.JobTimerTableAdapter();
+            this.tableAdapterManager1 = new WindowsFormsApplication1.Database1DataSet1TableAdapters.TableAdapterManager();
+            this.jobTimerDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.jobTimerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobTimerBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobTimerDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // database1DataSet1
+            // 
+            this.database1DataSet1.DataSetName = "Database1DataSet1";
+            this.database1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // jobTimerBindingSource1
+            // 
+            this.jobTimerBindingSource1.DataMember = "JobTimer";
+            this.jobTimerBindingSource1.DataSource = this.database1DataSet1;
+            // 
+            // jobTimerTableAdapter1
+            // 
+            this.jobTimerTableAdapter1.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager1
+            // 
+            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager1.JobTimerTableAdapter = this.jobTimerTableAdapter1;
+            this.tableAdapterManager1.UpdateOrder = WindowsFormsApplication1.Database1DataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // jobTimerDataGridView
+            // 
+            this.jobTimerDataGridView.AutoGenerateColumns = false;
+            this.jobTimerDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.jobTimerDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.jobTimerDataGridView.DataSource = this.jobTimerBindingSource1;
+            this.jobTimerDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.jobTimerDataGridView.Name = "jobTimerDataGridView";
+            this.jobTimerDataGridView.RowTemplate.Height = 31;
+            this.jobTimerDataGridView.Size = new System.Drawing.Size(857, 547);
+            this.jobTimerDataGridView.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Job Number";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Job Number";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Description";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Description";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Time";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Time";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 571);
+            this.Controls.Add(this.jobTimerDataGridView);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.jobTimerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobTimerBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobTimerDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource jobTimerBindingSource;
+        private Database1DataSet1 database1DataSet1;
+        private System.Windows.Forms.BindingSource jobTimerBindingSource1;
+        private Database1DataSet1TableAdapters.JobTimerTableAdapter jobTimerTableAdapter1;
+        private Database1DataSet1TableAdapters.TableAdapterManager tableAdapterManager1;
+        private System.Windows.Forms.DataGridView jobTimerDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].JobTimer
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [jobNumber] INT NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Time] INT NULL
+)
